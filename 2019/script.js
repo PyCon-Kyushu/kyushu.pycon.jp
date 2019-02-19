b5261adb1a2729f8f04d11ec555c67e1e841c1ef
hoto17296 <mail@hoto.me>
2019-02-19 23:32:30 +0900

    スマホのメニューを実装

--- 2019/script.js
+++ 2019/script.js
@@ -16,3 +16,11 @@ new StickyNavbar(window.document.querySelector('body > nav'), "fixed");
 
 // ページ内リンクをスクロールするやつ
 new SmoothScroll('a[href*="#"]', { offset: 100 });
+
+// スマホメニュー
+var menuModal = new PureModal(window.document.getElementById('nav-modal'));
+
+window.document.getElementById('nav-button-open').addEventListener('click', function(event) {
+  event.preventDefault();
+  menuModal.open();
+});
