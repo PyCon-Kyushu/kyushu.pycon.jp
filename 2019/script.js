12980d4d5e0a56a4355ef122d8457d798780b5a4
hoto17296 <mail@hoto.me>
2019-03-05 23:49:12 +0900

    スポンサーロゴを表示

--- 2019/script.js
+++ 2019/script.js
@@ -24,3 +24,28 @@ window.document.getElementById('nav-button-open').addEventListener('click', func
   event.preventDefault();
   menuModal.open();
 });
+
+// スポンサー
+axios.get('sponsors.json').then(function(res) {
+  var target = document.querySelector('#sponsors > div');
+  var ranks = {
+    'platinum': 'プラチナ',
+    'gold': 'ゴールド',
+    'silver': 'シルバー',
+  };
+  for (var rank of ['platinum', 'gold', 'silver']) {
+    var div = document.createElement('div');
+    div.classList.add(rank);
+    target.appendChild(div);
+    var h3 = document.createElement('h3');
+    h3.textContent = ranks[rank] + 'スポンサー';
+    div.appendChild(h3);
+    var ul = document.createElement('ul');
+    div.appendChild(ul);
+    for (var sponsor of res.data[rank]) {
+      var li = document.createElement('li');
+      li.style.backgroundImage = 'url(images/sponsors/' + sponsor.logo + ')';
+      ul.appendChild(li);
+    }
+  }
+});
